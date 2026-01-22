d6797eb540dcf7d0befc26450298d2997bc61579
natchi2026 <www.velunatchi@gmail.com>
2026-01-22 13:07:10 +0530

    Create name_Printer.java

---
+++ name_Printer.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class name_Printer {
+
+    public static void main(String[] args) {
+       
+     Scanner sc=new Scanner(System.in);
+       String name=sc.nextLine();
+       System.out.print("Hello\n"+name);
+    }
+}
